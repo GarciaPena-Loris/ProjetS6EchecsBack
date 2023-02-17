7c086441bed7ecc79ebfd58a51436ab9027b8776
Tom <romano.tom31@gmail.com>
2023-02-17 18:56:17 +0100

    je sais pas quoi mettre comme message

--- react-node-chess-app/front-chess-app/src/components/Accueil/Accueil.js
+++ react-node-chess-app/front-chess-app/src/components/Accueil/Accueil.js
@@ -3,15 +3,21 @@ import "../Components.css"
 import { Navigate, useNavigate } from "react-router-dom";
 import { redirect } from "react-router-dom";
 
+
 export default function Accueil() {
+  //fonction utile pour le router (plus particulièrement les boutons)
   const navigate = useNavigate();
+  //partie fonctionnel du bouton 'se connecter'
   function handleClick() {
     navigate("/connexion");
   }
 
   return (
     <div>
-      <h1>BLIND CHESS</h1>
+      <div>
+        <h1 className="titre">BLIND CHESS</h1>
+        <img src='../../public/imageAccueil.png' alt="imgAcceuil" width="377" height="377"></img>
+      </div>  
       <div className="divMargin">
         <button onClick={handleClick} className="button-4">Se connecter</button>
       </div>
