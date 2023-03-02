806061dbfebcc1680752ac51d594bc003f6019b9
PaulMedout-marere <paul.medout.m@gmail.com>
2023-03-02 18:04:10 +0100

    mini jeu nomenclature comparaison pas fini

--- react-node-chess-app/front-chess-app/src/components/Exercices/Nomenclature/Nomenclature.js
+++ react-node-chess-app/front-chess-app/src/components/Exercices/Nomenclature/Nomenclature.js
@@ -4,25 +4,45 @@ import '../../Components.css';
 import { useState } from 'react';
 import {Chessboard} from 'react-chessboard'
 import {Chess} from 'chess.js'
-              
+            
+
 function Nomenclature() {
-  const [game, setGame] = useState(new Chess());
-  
- 
+  const [chess] = useState(new Chess());
+  const colors = ['b','w'];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  const pieces = ['P', 'N', 'B', 'R', 'Q', 'K'];
+  const piece = pieces[Math.floor(Math.random() * pieces.length)];
+  const moves = chess.moves({ piece: piece });
+  var move = moves[Math.floor(Math.random() * moves.length)];
+  if (move.length>2){
+    move=move.substring(1);
+  }
+  console.log(move);
+  const movePieceObj = {
+    [move]: [color]+[piece]
+  };
+  console.log(movePieceObj);
+
+
+  const [inputValue, setInputValue] = React.useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    console.log(inputValue);
+  };
+
 
   return (
     <div className="container">
       <div className="chesscenter">
         <Chessboard
-            position="start"
-            width={400}
-            boardStyle={{
-              borderRadius: "5px",
-              boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`,
-              marginLeft: "50px",
-            }}
-          />
+          position={movePieceObj}
+          arePiecesDraggable={false}
+          width={400}
+        />
       </div>
+      <input id="saisieposition" type="text" value={inputValue} onChange={handleInputChange}></input>
+      <button id="checkposition">En es-tu bien sur?</button>
     </div>
   );
 }
