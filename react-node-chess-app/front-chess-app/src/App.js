28b0b8aa3f0e60e7e62b2da2dc0a3fe785c0cca9
Tom <romano.tom31@gmail.com>
2023-03-02 12:22:05 +0100

    page des exo

--- react-node-chess-app/front-chess-app/src/App.js
+++ react-node-chess-app/front-chess-app/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route } from 'react-router-dom';
 import Accueil from "./components/Accueil/Accueil";
 import Connexion from "./components/Connexion/Connexion";
 import Inscription from "./components/Inscription/Inscription";
+import SelectionExercices from "./components/SelectionExercices/SelectionExercices.js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Accueil/>} />
           <Route path="/connexion" element={<Connexion/>} />
           <Route path="/inscription" element={<Inscription/>} />
+          <Route path="/selectionExercices" element={<SelectionExercices/>}/>
       </Routes>
     </div>
   );
