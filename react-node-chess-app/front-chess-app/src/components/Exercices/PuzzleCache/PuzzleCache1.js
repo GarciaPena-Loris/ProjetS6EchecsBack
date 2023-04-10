2b7979fbd928faa2c8678a90b2f98db5d44b3c79
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2023-04-10 10:37:06 +0200

    petit modif

--- react-node-chess-app/front-chess-app/src/components/Exercices/PuzzleCache/PuzzleCache1.js
+++ react-node-chess-app/front-chess-app/src/components/Exercices/PuzzleCache/PuzzleCache1.js
@@ -9,7 +9,7 @@ import axios from "axios";
 
 //import { decodeToken } from "react-jwt";
 
-class PuzzleCache extends React.Component {
+class PuzzleCache1 extends React.Component {
     static SQUARES = [
         'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8',
         'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7',
@@ -380,4 +380,4 @@ class PuzzleCache extends React.Component {
     }
 }
 
-export default PuzzleCache;
+export default PuzzleCache1;
