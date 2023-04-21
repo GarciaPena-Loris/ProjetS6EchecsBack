dc08df7a3c5d505bb8dfbba674deeb0c9ca44e03
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2023-04-21 19:21:23 +0200

    change pool

--- models/database.js
+++ models/database.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: 'eu-cdbr-west-03.cleardb.net',
   user: 'b82a436c006eb8',
   password: process.env.DB_PASSWORD,
   database: 'heroku_1a3bccc94320ee7'
 });
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the MySQL database.');
-});
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected to the MySQL database.');
+// });
 
 module.exports = connection;
