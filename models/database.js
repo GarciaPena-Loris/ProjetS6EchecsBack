290071f22c1e573b1b223ec330becb09c37926e9
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2023-04-21 15:50:15 +0200

    gitignore

--- models/database.js
+++ models/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connection = mysql.createConnection({
   host: 'eu-cdbr-west-03.cleardb.net',
   user: 'b82a436c006eb8',
-  password: process.env.DB_PASSWORD,
+  password: '13e47e2f',
   database: 'heroku_1a3bccc94320ee7'
 });
 
